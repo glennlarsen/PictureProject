50d60109cf54428f09c5a91672d71bcbbe11f5de
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-11-04 14:12:28 +0100

    added S3 to routes and env

--- routes/pictures.js
+++ routes/pictures.js
@@ -1,25 +1,55 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const fs = require('fs');
-var path = require('path');
+const fs = require("fs");
+var path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
 
 /* GET pictures listing. */
-router.get('/', function(req, res, next) {
-    const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
-    res.render('pictures', { pictures: pictures});
-  });
+router.get("/", async (req, res, next) => {
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: "/",
+    Prefix: "public/",
+  };
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map((x) => x.Key);
 
-  router.get('/:pictureName', function(req, res, next) {
-    const pictureName = req.params.pictureName;
-    const pictures = fs.readdirSync(path.join(__dirname, `../pictures/`));
-    const filteredPictures = pictures.filter((pic) => pic ===  pictureName)
-    res.render('pictures', { pictures: filteredPictures});
-  });
+  const pictures = await Promise.all(
+    keys.map(async (key) => {
+      let my_file = await s3
+        .getObject({
+          Bucket: process.env.CYCLIC_BUCKET_NAME,
+          Key: key,
+        })
+        .promise();
+      return {
+        src: Buffer.from(my_file.Body).toString("base64"),
+        name: key.split("/").pop(),
+      };
+    })
+  );
+  res.render("pictures", { pictures: pictures });
+});
 
-router.post('/', function(req, res, next) {
-    const file = req.files.file;
-    fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
-    res.end();
-  });
+router.get("/:pictureName", function (req, res, next) {
+  const pictureName = req.params.pictureName;
+  const pictures = fs.readdirSync(path.join(__dirname, `../pictures/`));
+  const filteredPictures = pictures.filter((pic) => pic === pictureName);
+  res.render("pictures", { pictures: filteredPictures });
+});
+
+router.post("/", async function (req, res, next) {
+  const file = req.files.file;
+  console.log(req.files);
+  await s3
+    .putObject({
+      Body: file.data,
+      Bucket: process.env.CYCLIC_BUCKET_NAME,
+      Key: "public/" + file.name,
+    })
+    .promise();
+  res.end();
+});
 
 module.exports = router;
