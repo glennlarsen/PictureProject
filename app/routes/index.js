badf94bd6eecc804b4084c5c5b4569034c2690b2
Glenn Larsen <50579947+glennlarsen@users.noreply.github.com>
2023-11-09 15:35:49 +0100

    lowecase repo name

---
+++ app/routes/index.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var router = express.Router();
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+router.get("/", async function (req, res, next) {
+  var params = {
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Delimiter: "/",
+    Prefix: "public/",
+  };
+  var allObjects = await s3.listObjects(params).promise();
+  var keys = allObjects?.Contents.map((x) => x.Key).slice(0, 3);
+  const pictures = await Promise.all(
+    keys.map(async (key) => {
+      let my_file = await s3
+        .getObject({
+          Bucket: process.env.CYCLIC_BUCKET_NAME,
+          Key: key,
+        })
+        .promise();
+      return {
+        src: Buffer.from(my_file.Body).toString("base64"),
+        name: key.split("/").pop(),
+      };
+    })
+  );
+  res.render("index", {
+    pictures: pictures,
+    title: "Picture store",
+    isAuthenticated: req.oidc.isAuthenticated(),
+    user: req.oidc.user
+  });
+});
+
+module.exports = router;
